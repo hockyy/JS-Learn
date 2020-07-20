520b6ae76b7651ac790b6049cc7de343026d0189
Hocky Yudhiono <hocky.yudhiono@gmail.com>
2020-07-20 13:12:52 +0700

    Add promised navigator

--- async/app.js
+++ async/app.js
@@ -1,40 +1,62 @@
-const button = document.querySelector('button');
-const output = document.querySelector('p');
+const button = document.querySelector("button");
+const output = document.querySelector("p");
 
-const setTimer = (duration) => {
+const setTimer = duration => {
   const promise = new Promise((resolve, reject) => {
     // This will be executed right away
     // First argument is resolve, second argument is reject
     setTimeout(() => {
-      reject("No")
+      reject("No");
       resolve("Yes"); // This like returns
     }, duration);
   });
   return promise;
-}
+};
+
+const getPosition = opts => {
+  const promise = new Promise((resolve, reject) => {
+    navigator.geolocation.getCurrentPosition(
+      successReturnData => {
+        resolve(successReturnData);
+      },
+      failedReturnData => {
+        reject(failedReturnData);
+      },
+      opts
+    );
+  });
+  return promise;
+};
 
 function trackUserHandler() {
+  getPosition().then(successReturnData =>{
+    console.log("here");
+    console.log(successReturnData);
+  });
   navigator.geolocation.getCurrentPosition(
     posData => {
-      setTimer(2000).then(data =>{
-        console.log("Promise Fullfilled")
-        console.log(data);
-      }, data => {
-        console.log("Promise Rejected!")
-        console.log(data);
-      })
+      setTimer(2000).then(
+        data => {
+          console.log("Promise Fullfilled");
+          console.log(data);
+        },
+        data => {
+          console.log("Promise Rejected!");
+          console.log(data);
+        }
+      );
     },
     error => {
       console.log(error);
     }
   );
   setTimeout(() => {
-    console.log('Timer done!');
+    console.log("Timer done!");
   }, 0);
-  console.log('Getting position...');
+  console.log("Getting position...");
 }
 
-button.addEventListener('click', trackUserHandler);
+button.addEventListener("click", trackUserHandler);
 
 // let result = 0;
 
